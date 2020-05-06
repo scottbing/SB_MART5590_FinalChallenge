9b5ba81cfcb5c0cb15d44a231a18162efac51bd5
scottbing <sbing52@comcast.net>
2020-05-05 23:55:15 -0600

    SB_MART5590_FinalChallenge
    SB_MART5590_FinalChallenge

---
+++ UnityDemonstration/Assets/TimedSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class TimedSpawn : MonoBehaviour
+{
+
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public bool first_time = true;
+    public float spawnTime;
+    public float spawnDelay;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (first_time == true)
+        {
+            int milliseconds = 4000;
+            Thread.Sleep(milliseconds);
+            first_time = false;
+        }
+        Invoke("SpawnObject", 2);
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
